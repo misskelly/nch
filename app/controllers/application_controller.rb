6599e30a7ff7c9d4da1978310eccaa69d7ed378c
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 12:48:48 -0600

    Create require_admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,8 @@ class ApplicationController < ActionController::Base
     current_user && current_user.manager?
   end
 
+  def require_admin
+    render file: 'public/404', status: 404 unless current_admin?
+  end
+  
 end
