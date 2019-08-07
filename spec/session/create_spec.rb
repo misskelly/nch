b2abf937d975bc274259dafe59fbfe4ffd0cc71f
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 15:10:50 -0600

    Create test for admin login

--- spec/session/create_spec.rb
+++ spec/session/create_spec.rb
@@ -31,6 +31,19 @@ RSpec.describe "User can Login", type: :feature do
         expect(page).to have_content("Manager Dashboard Page")
       end
 
+      it "If I am an Admin User, I am redirected to my dashboard page" do
+        user = User.create!(name: "Sam", email: "email@gmail.com", password: "test", role: 2)
+
+        visit login_path
+
+        fill_in "User Name", with: "email@gmail.com"
+        fill_in "Password", with: "test"
+
+        click_button "Login"
+
+        expect(current_path).to eq(admin_dashboard_path)
+        expect(page).to have_content("Admin Dashboard Page")
+      end
     end
   end
 end
