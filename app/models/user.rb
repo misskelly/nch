c75f5e887a8359fc69441e442b602998eecbdcf4
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 13:32:15 -0600

    Create users model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates_presence_of :name,
+                        :role, presence: true
+
+  validates :email, uniqueness: true, presence: true
+
+  enum role: ["sales", "manager", "admin"]
+
+end
