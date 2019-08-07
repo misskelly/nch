4351c59be815a03fa117e2c927fc04dd464eca53
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 12:27:05 -0600

    Create method for current admin/manager

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
+  helper_method :current_user, :current_admin, :current_manager
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def current_manager?
+    current_user && current_user.manager?
+  end
+
 end
