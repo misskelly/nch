8e268f3020a5560268a8e5de2837ef8b0ecad430
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 13:04:38 -0600

    Change index to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#index', as: :home
+  get '/', to: 'welcome#show', as: :home
 
   get '/login', to: 'users#login', as: :login
   delete '/logout', to: 'users#logout', as: :logout
