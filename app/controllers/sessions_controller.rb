ca6c6bf01ac024db55e887aca8722ac148ca2780
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 14:17:07 -0600

    Move login/logout methods to sessions controller for clarity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,25 @@ class SessionsController < ApplicationController
       redirect_to login_path
     end
   end
+
+  def login
+    if current_admin?
+      redirect_to admin_dashboard_path
+      flash[:success] = "#{current_user.name} is logged in."
+    elsif current_manager?
+      redirect_to manager_dashboard_path
+      flash[:success] = "#{current_user.name} is logged in."
+    elsif current_user
+      redirect_to user_dashboard_path
+      flash[:success] = "#{current_user.name} is logged in."
+    end
+  end
+
+  def logout
+    session.delete(:cart)
+    flash[:success] = "#{current_user.name} is now logged out!"
+    session[:user_id] = nil
+    redirect_to home_path
+  end
+
 end
