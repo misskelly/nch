08cdc45bbecaf9232b9409b5aeeeb14f21a60425
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 15:05:50 -0600

    Update routes for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#show', as: :home
 
-  get '/login', to: 'users#login', as: :login
-  delete '/logout', to: 'users#logout', as: :logout
+  get '/login', to: 'sessions#create', as: :login
+  delete '/logout', to: 'sessions#destroy', as: :logout
   post '/login', to: 'sessions#create'
 
   get '/admin/dashboard', to: 'admin/dashboard#show', as: :admin_dashboard
