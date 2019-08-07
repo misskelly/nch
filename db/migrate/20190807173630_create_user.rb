0cfb6314aa8460dbfa93b66e494cfc8ea17f5bac
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-07 15:06:14 -0600

    Update password to password_digest for bcrypt

--- db/migrate/20190807173630_create_user.rb
+++ db/migrate/20190807173630_create_user.rb
@@ -3,7 +3,7 @@ class CreateUser < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.integer :role
 
       t.timestamps
