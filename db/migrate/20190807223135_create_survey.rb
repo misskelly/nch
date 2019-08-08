7816796f2af92b3a4326365b86b1d5ac60428505
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-08 13:47:41 -0600

    Update tables for integer values rather than strings

--- db/migrate/20190807223135_create_survey.rb
+++ db/migrate/20190807223135_create_survey.rb
@@ -4,16 +4,16 @@ class CreateSurvey < ActiveRecord::Migration[5.2]
       t.string :location
       t.integer :reservation_number
       t.string :date_of_survey
-      t.string :professional_associates
-      t.string :prompt_response
-      t.string :appropriate_recommendation
-      t.string :appropriate_instruction_paperwork
-      t.string :move_in_experience
-      t.string :clean_and_comfortable
-      t.string :appropriate_furnishing
-      t.string :bed_and_bedding
-      t.string :would_use_again
-      t.string :would_recommend
+      t.integer :professional_associates
+      t.integer :prompt_response
+      t.integer :appropriate_recommendation
+      t.integer :appropriate_instruction_paperwork
+      t.integer :move_in_experience
+      t.integer :clean_and_comfortable
+      t.integer :appropriate_furnishing
+      t.integer :bed_and_bedding
+      t.integer :would_use_again
+      t.integer :would_recommend
 
       t.timestamps
     end
