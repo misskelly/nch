0faabc54b00ab52cf1a1630a62d4997a59f80a7e
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-18 19:26:04 -0600

    Create test for admin dashboard page

---
+++ spec/features/admin/show_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "Admin Dashboard Page", type: :feature do
+  describe 'As an admin' do
+    before :each do
+      @user = User.create!(name: "Becca", email: "email@gmail.com", password: "test", role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it 'I see my dashboard page' do
+      visit admin_dashboard_path
+      expect(page).to have_content("Admin Dashboard Page")
+    end
+  end
+end
